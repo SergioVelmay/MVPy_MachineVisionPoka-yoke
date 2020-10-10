e90feecbda513bec737a9e0c7afae20a9cd40362
Sergio Velasco <sergiovelmay@gmail.com>
2020-10-10 21:45:47 +0200

    Constant variables renamed and updated.

--- YWIL_Constants.py
+++ YWIL_Constants.py
@@ -1,7 +1,10 @@
 # validation and detection box colors
+welcome = "YWIL - YOU WORK, IT LOOKS. LET'S START THE ASSEMBLY!"
+
+# validation and detection box colors
 colors = {
-    'red': '#ec1f24',
     'green': '#0ea84b',
+    'red': '#ec1f24',
     'gray': '#868686'}
 
 # classification multilabel steps constants
@@ -18,7 +21,7 @@ multilabel_labels = [
     "Step7",
     "right",
     "wrong"]
-multilabel_help_messages = [
+multilabel_help = [
     "Multilabel classification from 0 to 4 ready",
     "Waiting for the white base to be placed...",
     "Place the first part in the rectangular hole",
@@ -26,7 +29,7 @@ multilabel_help_messages = [
     "Place the pink part with the hole facing up",
     "Place the gray rotor on top of the pink part",
     "Well done! Let's continue with the next steps"]
-multilabel_error_messages = [
+multilabel_error = [
     "Flip the pink part, it is placed backwards",
     "Remove the previous assembly from the base"]
 
@@ -37,12 +40,12 @@ detection_labels = [
     "Step5.error",
     "Step6.hole",
     "Step6.part"]
-detection_help_messages = [
+detection_help = [
     "Object Detection model for steps 5 and 6 ready",
     "Place the green parts in the four square holes",
     "Place the orange part in the central round hole",
     "Congratulations! Let's move on to the last step"]
-detection_error_messages = [
+detection_error = [
     "Remove the marked part, it is in the wrong hole"]
 
 # classification multiclass steps constants
@@ -51,10 +54,10 @@ multiclass_labels = [
     "Step7.true",
     "Step7.false.A",
     "Step7.false.B"]
-multiclass_help_messages = [
+multiclass_help = [
     "Multiclass classification for step 7 ready",
     "Place the gray gear on top of the mechanism",
     "Very good job, human! Assembly completed!"]
-multiclass_error_messages = [
+multiclass_error = [
     "Flip the gray gear, it is placed backwards",
     "Rotate the gear a bit until it fits properly"]
