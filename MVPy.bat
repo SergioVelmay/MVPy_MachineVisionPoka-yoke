67924f3b7b8320cd13dda284f9aee6464c36132d
SergioVelmay <42042494+SergioVelmay@users.noreply.github.com>
2021-02-14 19:36:45 +0100

    NCS2 project configuration. (#2)
    * Python classes refactorized.
    
    * Model managers refactorized.
    
    * Main file updated.
    
    * Launcher script created.

---
+++ MVPy.bat
@@ -0,0 +1,5 @@
+cd C:\Program Files (x86)\IntelSWTools\openvino\bin\
+call setupvars.bat
+cd C:\Users\sergi\source\repos\MVPy_MachineVisionPoka-yoke
+py MVPy.py
+cmd /k
---
+++ MulticlassClassification.py
@@ -0,0 +1,67 @@
+import cv2
+import numpy as np
+from openvino.inference_engine import IECore
+from Classes import Classification
+
+class MulticlassClassification:
+
+    PROB_THRESHOLD = 0.7
+    MAX_DETECTIONS = 1
+
+    def __init__(self, inference_engine):
+        model_path = 'MulticlassClassification/model'
+        model_xml = model_path + '.xml'
+        model_bin = model_path + '.bin'
+        model_labels = model_path + '.labels'
+        # device_name = 'CPU'
+        device_name = 'MYRIAD'
+
+        with open(model_labels, 'r') as io:
+            self.labels = [x.split(sep=' ', maxsplit=1)[-1].strip() for x in io]
+
+        # inference_engine = IECore()
+
+        network = inference_engine.read_network(model=model_xml, weights=model_bin)
+
+        network.batch_size = 1
+
+        self.input_blob = next(iter(network.inputs))
+        self.output_blob = next(iter(network.outputs))
+
+        n, c, h, w = network.inputs[self.input_blob].shape
+
+        self.exec_network = inference_engine.load_network(network=network, device_name=device_name)
+
+        self.images = np.ndarray(shape=(n, c, h, w))
+
+    def Infer(self, image):
+        self.images[0] = self._preprocess(image)
+
+        result = self.exec_network.infer(inputs={self.input_blob: self.images})
+
+        outputs = result[self.output_blob]
+
+        return self._postprocess(outputs)
+
+    def _preprocess(self, image):
+        crop_image = image[0:480, 80:560]
+
+        resized_image = cv2.resize(crop_image, (224, 224), interpolation=cv2.INTER_AREA)
+
+        input_image = resized_image.transpose((2, 0, 1))
+
+        return input_image
+
+    def _postprocess(self, outputs):
+        predictions = list()
+        
+        for probs in outputs:
+            probs = np.squeeze(probs)
+            top_ind = np.argsort(probs)[-self.MAX_DETECTIONS:][::-1]
+            for id in top_ind:
+                if probs[id] > self.PROB_THRESHOLD:
+                    prob = probs[id] * 100
+                    prediction = Classification(self.labels[id], prob)
+                    predictions.append(prediction)
+
+        return predictions
