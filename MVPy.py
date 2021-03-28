4ff8657dc4dc1c7067f6b60538a2aea8fcdc12fe
Sergio Velmay <sergiovelmay@gmail.com>
2021-03-28 22:19:43 +0200

    Part counting logic and message updated.

--- MVPy.py
+++ MVPy.py
@@ -13,12 +13,16 @@ import argparse
 parser = argparse.ArgumentParser(
     prog='MVPy | Machine Vision Poka-yoke', 
     description='Edge computing application for manual assembly cells.', 
-    epilog='example: $ py MVPy.py -d MYRIAD -t False')
+    epilog='example: $ py MVPy.py -d MYRIAD -t False -z False')
 parser.add_argument('-d', '--device', 
     default='MYRIAD', 
     choices=['CPU', 'GPU', 'HDDL', 'MYRIAD'], 
     help='device name for OpenVINO inference')
 parser.add_argument('-t', '--training', 
+    default='False', 
+    choices=['False', 'True', 'No', 'Yes', '0', '1'],  
+    help='store image captures for training')
+parser.add_argument('-z', '--zooms', 
     default='False', 
     choices=['False', 'True', 'No', 'Yes', '0', '1'],  
     help='store image captures for training')
@@ -28,6 +32,8 @@ args = parser.parse_args()
 device_name = args.device
 
 training_captures = string_to_boolean(args.training)
+
+training_zooms = string_to_boolean(args.training)
 
 from tkinter import Tk, Label
 from PIL import ImageTk, Image
@@ -44,7 +50,7 @@ window = Window()
 print('[ MVPy ] Graphical Interface loaded')
 
 import Constants
-from Constants import colors_hex, colors_bgr
+from Constants import colors_hex, colors_bgr, welcome
 from Constants import multilabel_labels, multilabel_help, multilabel_error
 from Constants import detection_labels, detection_help, detection_error
 from Constants import multiclass_labels, multiclass_help, multiclass_error
@@ -101,16 +107,30 @@ for model in range(number_of_models):
 
 waiting_millis = 1
 waiting_frames = 30
-min_validations = 15
 
+min_validations = 30
+
+welcome_message = welcome
 welcome_waiting = 0
+
 current_step = 0
-current_model = 999
+current_model = 0
 current_message = 0
 
 assembly_completed = False
+
 gloves_missing = False
-gloves_tracking = False
+
+parts_counting = True
+part_counts_ok = 0
+part5_max = 4
+part6_max = 1
+
+oring_tracking = False
+oring_counts_ok = 0
+
+glove_tracking = False
+glove_count_ok = 0
 
 frame_number = 0
 
@@ -118,26 +138,25 @@ import logging
 
 logging.basicConfig(format='[ %(levelname)s ] New Video Capture | Frame Count %(message)s | %(asctime)s', level=logging.INFO)
 
-training_folder = 'C:/Users/sergi/Desktop/MVPy/TrainingSet/'
+# training_folder = 'C:/Users/sergi/Desktop/MVPy/TrainingSet/'
 
-videos_folder = training_folder + 'Videos/'
-images_folder = training_folder + 'Images/Part_Count/'
+# videos_folder = training_folder + 'Videos/'
+# images_folder = training_folder + 'Images/Part_Count/'
 
-zooms_folder = training_folder + 'Zooms/Part4_Glove_R/'
-training_zooms = False
+# zooms_folder = training_folder + 'Zooms/Part4_Glove_R/'
 
-import os
+# import os
 
-if not os.path.exists(images_folder):
-    os.makedirs(images_folder)
+# if not os.path.exists(images_folder):
+#     os.makedirs(images_folder)
 
-if not os.path.exists(zooms_folder):
-    os.makedirs(zooms_folder)
+# if not os.path.exists(zooms_folder):
+#     os.makedirs(zooms_folder)
 
 # video_capture = cv2.VideoCapture(0)
 # video_capture = cv2.VideoCapture(1)
-# video_capture = cv2.VideoCapture(2)
-video_capture = cv2.VideoCapture(4)
+video_capture = cv2.VideoCapture(2)
+#video_capture = cv2.VideoCapture(4)
 # video_capture = cv2.VideoCapture('Videos/MVPy_Assembly_640x480.mp4')
 # video_capture = cv2.VideoCapture(videos_folder + 'Part4_Glove_R.mp4')
 
@@ -150,39 +169,46 @@ def video_streaming():
     global video_capture
     _, frame = video_capture.read()
     image = cv2.resize(frame, (640, 480), interpolation=cv2.INTER_AREA)
-    if training_captures:
-        if frame_number % 6 == 0:
-            image_crop = image[0:480, 80:560]
-            image_name = f'{images_folder}Part_Count_{frame_number:06}.jpg'
-            cv2.imwrite(image_name, image_crop)
+    # if training_captures:
+    #     if frame_number % 6 == 0:
+    #         image_crop = image[0:480, 80:560]
+    #         image_name = f'{images_folder}Training_Step_{frame_number:06}.jpg'
+    #         cv2.imwrite(image_name, image_crop)
+    global welcome_message
     global welcome_waiting
+    global parts_counting
+    global oring_tracking
+    global glove_tracking
     global gloves_missing
     if welcome_waiting > waiting_frames:
         image_crop = image[0:480, 80:560]
         predictions = []
         time_inference_start = datetime.now().microsecond
-        if current_model == 999:
+        if parts_counting:
             predictions = part_count.Infer(image_crop)
-        if current_model == 0:
-            predictions = multilabel.Infer(image_crop)
-        elif current_model == 1:
-            predictions = detection.Infer(image_crop)
-        elif current_model == 2:
-            predictions = multiclass.Infer(image_crop)
-        elif current_model == 3:
+        elif oring_tracking:
             predictions = part_4_det.Infer(image_crop)
+        else:
+            if current_model == 0:
+                predictions = multilabel.Infer(image_crop)
+            elif current_model == 1:
+                predictions = detection.Infer(image_crop)
+            elif current_model == 2:
+                predictions = multiclass.Infer(image_crop)    
+        if glove_tracking:
+            tracking = glove_class.Infer(image_crop)
+            if len(tracking) > 0:
+                if 'Negative' not in tracking[0].Label:
+                    predictions.append(tracking[0])
+                if 'Hand' in tracking[0].Label:
+                    gloves_missing = True
         time_inference_end = datetime.now().microsecond
-        if current_model == 999:
+        if parts_counting:
             validations = process_part_count(predictions)
             image = draw_part_count(image, validations)
-        if current_model == 0:
-            process_multilabel(predictions)
-        elif current_model == 1:
-            detections = process_detection(predictions)
-            image = draw_detections(image, detections)
-        elif current_model == 2:
-            process_multiclass(predictions)
-        elif current_model == 3:
+            window.assembly.config(image=assembly_images[6])
+            window.currently.config(text='Part counting detections:')
+        elif oring_tracking:
             detections = process_part_4_det(predictions)
             image = draw_detections(image, detections)
             if len(detections) > 0:
@@ -197,7 +223,7 @@ def video_streaming():
                 if image_zoom.size != 0:
                     validations = oring_class.Infer(image_zoom)
                     if len(validations) > 0:
-                        predictions.a(validations[0])
+                        predictions.append(validations[0])
                         if 'True' in validations[0].Label:
                             color = colors_bgr['yes']
                         else:
@@ -205,21 +231,23 @@ def video_streaming():
                         text = 'O-Ring'
                         cv2.putText(image, text, (x1 + 85, y1 + 25), fontFace=cv2.FONT_HERSHEY_DUPLEX,  
                             fontScale=0.9, thickness=2, color=color, bottomLeftOrigin=False)
-                    if training_zooms:
-                        zoom_name = f'{zooms_folder}Part4_Glove_R_{frame_number:06}.jpg'
-                        cv2.imwrite(zoom_name, image_zoom)
-        
-        if gloves_tracking:
-            tracking = glove_class.Infer(image_crop)
-            if len(tracking) > 0:
-                predictions.a(tracking[0])
-                if 'Hand' in tracking[0].Label:
-                    gloves_missing = True
-        window.assembly.config(image=assembly_images[current_step])
-        print_currently(len(predictions))
+                    # if training_zooms:
+                    #     zoom_name = f'{zooms_folder}Training_Zoom_{frame_number:06}.jpg'
+                    #     cv2.imwrite(zoom_name, image_zoom)
+        else:
+            if current_model == 0:
+                process_multilabel(predictions)
+            elif current_model == 1:
+                detections = process_detection(predictions)
+                image = draw_detections(image, detections)
+            elif current_model == 2:
+                process_multiclass(predictions)
+            window.assembly.config(image=assembly_images[current_step])
+            print_currently(len(predictions))
         print_detections(predictions)
         print_inference_time(time_inference_start, time_inference_end)
     else:
+        write_instruction(welcome_message)
         welcome_waiting = welcome_waiting + 1
     global assembly_completed
     if assembly_completed:
@@ -239,11 +267,12 @@ def video_streaming():
 
 def process_multilabel(predictions):
     global current_message
+    global oring_tracking
     message = multilabel_help[current_message]
     checking_value = 'aux'
     detected_labels = []
     for prediction in predictions:
-            detected_labels.a(prediction.Label)
+        detected_labels.append(prediction.Label)
     if current_message != 0:
         if multilabel_labels[5] in detected_labels:
             checking_value = 'no'
@@ -261,6 +290,7 @@ def process_multilabel(predictions):
                 if condition_1 and condition_2:
                     update_message()
                     update_progress()
+                    oring_tracking = True
                 elif condition_1:
                     checking_value = 'aux'
             for label in detected_labels:
@@ -374,9 +404,23 @@ def process_part_4_det(predictions):
     return selected_predictions
 
 def process_part_count(predictions):
+    global part5_max
+    global part6_max
+    part5_count = 0
+    part6_count = 0
     if len(predictions) > 0:
-        checking_value = 'yes'
         predictions = order_predictions(predictions)
+        checking_value = 'yes'
+        for prediction in predictions:
+            if 'False' in prediction.Label:
+                checking_value = 'no'
+            else:
+                if '5.T' in prediction.Label:
+                    part5_count = part5_count + 1
+                elif '6.T' in prediction.Label:
+                    part6_count = part6_count + 1
+        if part5_count > part5_max or part6_count > part6_max:
+            checking_value = 'no'
     else:
         checking_value = 'aux'
     validations = make_validations(predictions)
@@ -388,9 +432,12 @@ def order_predictions(predictions):
     return predictions
 
 def make_validations(predictions):
-    part5_max = 4
+    global min_validations
+    global parts_counting
+    global part_counts_ok
+    global part5_max
+    global part6_max
     part5_count = 0
-    part6_max = 1
     part6_count = 0
     validations = []
     for prediction in predictions:
@@ -407,21 +454,32 @@ def make_validations(predictions):
                 part5_count = part5_count + 1
                 if part5_count > part5_max:
                     color = colors_bgr['no']
-                    thickness = 6
-                else:
-                    text = f'{part5_count}/{part5_max}'
+                text = f'{part5_count}/{part5_max}'
             elif '6' in prediction.Label:
                 part6_count = part6_count + 1
                 if part6_count > part6_max:
                     color = colors_bgr['no']
-                    thickness = 6
-                else:
-                    text = f'{part6_count}/{part6_max}'
-
+                text = f'{part6_count}/{part6_max}'
         validation = Validation(prediction.Label, prediction.Probability, 
             prediction.Box.Left, prediction.Box.Top, prediction.Box.Width, prediction.Box.Height, 
             color, thickness, text)
         validations.append(validation)
+    part5_total = part5_max - part5_count
+    part6_total = part6_max - part6_count
+    part5_prefix = ''
+    part6_prefix = ''
+    if part5_total > 0:
+        part5_prefix = '+'
+    if part6_total > 0:
+        part6_prefix = '+'
+    message = f'{part5_prefix}{part5_total} GREEN AND {part6_prefix}{part6_total} ORANGE PARTS NEEDED TO START'
+    if part5_count == part5_max and part6_count == part6_max:
+        part_counts_ok = part_counts_ok + 1
+    else:
+        part_counts_ok = 0
+    if part_counts_ok == min_validations:
+        parts_counting = False
+    window.instruction.config(text=message)
     return validations
 
 def validate_step():
@@ -514,13 +572,9 @@ def draw_detections(image, predictions):
 def draw_part_count(image, validations):
     for validation in validations:
         image = draw_rectangle(image, validation.Detection.Box, validation.Color, validation.Thickness)
-        x1 = int(round(validation.Detection.Box.Left * 480))
-        xw = int(round(validation.Detection.Box.Width * 480))
-        x2 = x1 + xw
-        y1 = int(round(validation.Detection.Box.Top * 480))
-        yh = int(round(validation.Detection.Box.Height * 480))
-        y2 = y1 + yh
-        cv2.putText(image, validation.Text, (x1 + 85, y1 + 25), fontFace=cv2.FONT_HERSHEY_DUPLEX,  
+        x = int(round(validation.Detection.Box.Left * 480))
+        y = int(round(validation.Detection.Box.Top * 480))
+        cv2.putText(image, validation.Text, (x + 85, y + 25), fontFace=cv2.FONT_HERSHEY_DUPLEX,  
             fontScale=0.9, thickness=2, color=validation.Color, bottomLeftOrigin=False)
     return image
 
